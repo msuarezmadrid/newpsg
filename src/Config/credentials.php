072633dcc666b560788b0aad1e7c8d0b1eecb240
msuarezmadrid <msuarezmadrid@gmail.com>
2025-08-05 18:27:43 -0400

    Version estable Agosto2025 v2

---
+++ src/Config/credentials.php
@@ -0,0 +1,56 @@
+<?php 
+    $credentials = [
+        "logs" => [
+            "tmp_log"=>"/var/log/psg/web_log",
+            "login_log"=>"/var/log/psg/login_log"
+        ],
+        "local" => [
+            "user" => "cm9vdA==",
+            "pass" => "TXN1NHIzei4sMTk4NQ==",
+            "host" => "bG9jYWxob3N0",
+            "db" => "intradb"
+        ],
+        "intradb" => [
+            "user" => "YWxsd2FyZQ==",
+            "pass" => "eEAzRHJaNWc1aWpOR3pf",
+            // "host" => "MTcyLjE4LjE1NS4xNjY=" # ...166
+			"host" => "bG9jYWxob3N0", # localhost 
+			"db"   => "intradb"
+        ],
+        "o_m" => [
+            "user" => "YWxsd2FyZQ==",
+            "pass" => "eEAzRHJaNWc1aWpOR3pf",
+            "host" => "MTcyLjE4LjE1NS4xNjY=",
+			"db"   => "o_m"
+        ],
+        "dbhudson" => [
+            "user" => "cm9vdA==",
+            "pass" => "M24xZ200",
+            "host" => "MTkyLjE2OC4zMy4yMzc="
+        ],
+        "sshhudson" => [
+            "user" => "YWxsd2FyZQ==",
+            "pass" => "YWxsd2FyZTIwMDk="
+        ],
+        "CONCELDAS" => [
+            "user" => "Y29uY2VsZGFz",
+            "pass" => "YzBuYzNsZDRzLjk4Nw==",
+            "host" => "MTcyLjE4LjE1NS4yMjg="
+        ],
+        "celdas" => [
+            "user" => "cmNlbGRhcw==",
+            "pass" => "Y2VsZGFzcg==",
+            "host" => "MTcyLjE4LjE1NS4xMzI="
+        ],
+
+        "oracleSiebel" => [
+            "user" => "Y29uc19hcHM=",
+            "pass" => "VG9tYWxhMjA=",
+            "host" => "MTcyLjE2LjEwNS4xOToxNTIxL09DRE1QUjAxX0NEVg=="
+        ],
+        "oracleSiebel" => [
+            "user" => "RkVSTkFORE9fSk9SUVVFUkE=",
+            "pass" => "ZmpvcnF1ZXJhMjAyMV8=",
+            "host" => "MTcyLjE2LjEwNS4yNToxNTIxL29jcmVwcm9kLnNuZGJhZDEudGRlY2xwcm9kLm9yYWNsZXZjbi5jb20="
+        ]
+    ];
